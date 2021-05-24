7e1282a2e6420e4837d0a41b8743a00f7441dd01
GrDaniel <daniells92@o2.pl>
2021-05-24 22:36:03 +0200

    the first version of automatic deploy script and requirements

--- dev/deploy.sh
+++ dev/deploy.sh
@@ -1,4 +1,13 @@
 #! /bin/bash
 
+# Active virtualenv
 source venv/bin/activate
+
+# Copy files
 rshell --buffer-size=30 -p /dev/ttyUSB0 -a rsync -m ../src/ /pyboard
+
+# List files
+rshell -p /dev/ttyUSB0 ls -all /pyboard/
+
+# Soft Reset
+pyboard --device /dev/ttyUSB0 -c 'import sys; sys.exit()'
--- src/boot.py
+++ src/boot.py
@@ -1,1 +1,11 @@
 #  This is a template for boot file.
+from machine import UART, freq
+from time import sleep
+uart = UART(0, 115200)
+uart.init(115200, bits=8, parity=None, stop=1)
+
+current_freq = freq()
+
+uart.write('********************************')
+uart.write(current_freq)
+uart.write('********************************')
