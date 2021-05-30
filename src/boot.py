c4054263dca3330dc62231a1099060f5dbb86fe9
GrDaniel <daniells92@o2.pl>
2021-05-30 22:17:40 +0200

    ds1307 RTC lib

--- src/boot.py
+++ src/boot.py
@@ -1,11 +1,19 @@
-#  This is a template for boot file.
-from machine import UART, freq
+# #  This is a template for boot file.
+# from machine import UART, freq
+# from time import sleep
+# uart = UART(0, 115200)
+# uart.init(115200, bits=8, parity=None, stop=1)
+#
+# current_freq = freq()
+#
+# uart.write('********************************')
+# uart.write(current_freq)
+# uart.write('********************************')
+from machine import Pin, UART
 from time import sleep
+
 uart = UART(0, 115200)
 uart.init(115200, bits=8, parity=None, stop=1)
 
-current_freq = freq()
-
-uart.write('********************************')
-uart.write(current_freq)
-uart.write('********************************')
+sleep(2)
+uart.write('WYSTARTOWAL')
--- src/main.py
+++ src/main.py
@@ -1,11 +1,20 @@
-from machine import Pin
-from time import sleep
+# RTC: DS1307
+# i2c scan() ==> 104 ==> 0x68
 
-led_pin = Pin(2, Pin.OUT)
+#from machine import Pin, UART
+#from time import sleep
+#
+#uart = UART(0, 115200)
+#uart.init(115200, bits=8, parity=None, stop=1)
+#
+#while True:
+#    uart.write('WYSTARTOWAL')
+#    sleep(5)
 
+##################################################
+from machine import I2C, Pin
+import ds1307
 
-while True:
-    led_pin.on()
-    sleep(5)
-    led_pin.off()
-    sleep(2)
+i2c = I2C(scl=Pin(5), sda=Pin(4), freq=100000)
+i2c.scan()
+##################################################
